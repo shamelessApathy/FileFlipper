ebc88ad36ff2a05516e272978c3bcd79aa7c367f
B <bcm811@gmail.com>
2018-05-24 12:38:16 -0600

    changed format of fileController around a bit, forgot to install imagicka nd composer,t hats what it wasnt working

--- controllers/fileController.php
+++ controllers/fileController.php
@@ -2,25 +2,35 @@
 class fileController {
 	public function sort()
 	{
+		echo "POST: <br>";
 		var_dump($_POST);
 		echo "<br>";
+		echo "FILE:<br>";
 		var_dump($_FILES);
 		$fileType = $_POST['type'];
-		if ($fileType === 'image')
-		{
-			$conv = $_POST['image-type'];
-		}
 		$file = $_FILES['file'];
 		$type = $file['type'];
 		$name = $file['name'];
 		$tmp_name = $file['tmp_name'];
 		if ($fileType === 'image')
 		{
+			$conv = $_POST['image-type'];
 			$image = new Imagick($tmp_name);
+			if ($image)
+			{
+				echo "imagick working";
+			}
 			$splode = explode('/', $tmp_name);
 			$tmp_name = $splode[1];
-			$image_string = "/var/www/FileFLipper/testing/" . $tmp_name . "." . $conv;
-			$image->writeImage($image_string);
+			$image_string = "/var/www/FileFlipper/testing/" . $tmp_name . "." . $conv;
+			if ($image->writeImage($image_string))
+			{
+				echo "it saved!";
+			}
+			else
+			{
+				echo "something went wrong!";
+			}
 		}
 		$link = "/testing/" . $tmp_name . "." . $conv;
 		echo "<ul>
